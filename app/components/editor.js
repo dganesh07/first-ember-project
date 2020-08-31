b0257d21a0c415808f815d477c173bd9735198db
Damini Ganesh <damini.ganesh@gmail.com>
2020-08-31 16:50:34 -0400

    New random string on reload

--- app/components/editor.js
+++ app/components/editor.js
@@ -1,7 +1,17 @@
 import Component from "@glimmer/component";
 import { htmlSafe } from "@ember/template";
 import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class EditorComponent extends Component {
-  @tracked testValue = htmlSafe("<div>BOOO</div>");
+  @tracked testValue = htmlSafe(
+    `<div> ${Math.random().toString(36).substring(7)}</div>`
+  );
+
+  //   @action
+  //   getContent() {
+  //     this.testValue = htmlSafe(
+  //       `<div> ${Math.random().toString(36).substring(7)} </div>`
+  //     );
+  //   }
 }
