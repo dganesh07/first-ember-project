05a9782dcf12aa5858006a3fdca72274a1c1a00d
Damini Ganesh <damini.ganesh@gmail.com>
2020-08-31 16:51:04 -0400

    No need for this action

--- app/components/editor.js
+++ app/components/editor.js
@@ -7,11 +7,4 @@ export default class EditorComponent extends Component {
   @tracked testValue = htmlSafe(
     `<div> ${Math.random().toString(36).substring(7)}</div>`
   );
-
-  //   @action
-  //   getContent() {
-  //     this.testValue = htmlSafe(
-  //       `<div> ${Math.random().toString(36).substring(7)} </div>`
-  //     );
-  //   }
 }
