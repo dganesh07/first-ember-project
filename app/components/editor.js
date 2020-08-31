377952d6e225bc2eba43d4447b890e66c187dd98
Damini Ganesh <damini.ganesh@gmail.com>
2020-08-31 18:58:43 -0400

    using random word npm package to control min and max words

--- app/components/editor.js
+++ app/components/editor.js
@@ -3,8 +3,10 @@ import { htmlSafe } from "@ember/template";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import randomParagraph from "random-paragraph";
+import randomWords from "random-words";
 
 export default class EditorComponent extends Component {
-  @tracked testValue = htmlSafe(`<div> ${randomParagraph()}</div>`);
-
+  @tracked testValue = htmlSafe(
+    `<div> ${randomWords({ min: 50, max:300, join: " " })}</div>`
+  );
 }
