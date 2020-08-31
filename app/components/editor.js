4dc74b31e45bc2201f8514d861d473708b8d5df0
Damini Ganesh <damini.ganesh@gmail.com>
2020-08-31 19:15:50 -0400

    Duplicating editor components 5 times

--- app/components/editor.js
+++ app/components/editor.js
@@ -1,12 +1,22 @@
 import Component from "@glimmer/component";
 import { htmlSafe } from "@ember/template";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-import randomParagraph from "random-paragraph";
 import randomWords from "random-words";
 
 export default class EditorComponent extends Component {
-  @tracked testValue = htmlSafe(
-    `<div> ${randomWords({ min: 50, max:300, join: " " })}</div>`
-  );
+  @tracked editorOne = this.generateRandomParagraph();
+  @tracked editorTwo = this.generateRandomParagraph();
+  @tracked editorThree = this.generateRandomParagraph();
+  @tracked editorFour = this.generateRandomParagraph();
+  @tracked editorFive = this.generateRandomParagraph();
+
+  generateRandomParagraph() {
+    return htmlSafe(
+      `<div> ${randomWords({
+        min: 50,
+        max: 300,
+        join: " ",
+      })}</div>`
+    );
+  }
 }
